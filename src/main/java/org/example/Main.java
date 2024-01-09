e876df2fde933a4b6ae7834fa8c2afb11be01883
KlyukinSA <ctepa.kl@mail.ru>
2024-01-09 11:09:54 +0300

    speed up algo, use hashmaps

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,68 +10,73 @@ public class Main {
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(args[0]));
-            ArrayList<Group> groups = new ArrayList<>();
+            List<List<String>> groups = new ArrayList<>();
+            List<Map<String, Integer>> parts = new ArrayList<>();
+
             String line = reader.readLine();
             while (line != null) {
                 String[] rows = line.split(";");
-                List<String> ints = new ArrayList<>();
-                for (String row : rows) {
-                    try {
-                        ints.add(row.replaceAll("\"", ""));
-                    }
-                    catch (NumberFormatException exception) {
-                        ints.add(null);
-                    }
+                for (int i = 0; i < rows.length; i++) {
+                    rows[i] = rows[i].replaceAll("\"", "");
                 }
-                boolean belongs = false;
-                for (Group group : groups) {
-                    if (belongs) {
-                        break;
-                    }
-                    List<Set<String>> parts = group.getParts();
-                    for (int i = 0; i < Math.min(parts.size(), rows.length); i++) {
-                        Set<String> possibleRowValues = parts.get(i);
-                        String value = ints.get(i);
-                        if (possibleRowValues.contains(value)) {
-                            group.getLines().add(line);
-                            apply(ints, parts);
-                            belongs = true;
-                            break;
+                Integer groupNumber = null;
+                for (int i = 0; i < Math.min(parts.size(), rows.length); i++) {
+                    String value = rows[i];
+                    if (!value.isEmpty()) {
+                        Integer groupNumber2 = parts.get(i).get(value);
+                        if (groupNumber != null && groupNumber2 != null && !groupNumber2.equals(groupNumber)) {
+                            for (String val : groups.get(groupNumber2)) {
+                                groups.get(groupNumber).add(val);
+                            }
+                        }
+                        if (groupNumber == null) {
+                            groupNumber = groupNumber2;
                         }
                     }
                 }
-                if (!belongs && ints.stream().anyMatch(s -> !s.isEmpty())) {
-                    List<Set<String>> parts = new ArrayList<>();
-                    apply(ints, parts);
-                    groups.add(Group.builder()
-                            .lines(new ArrayList<>(Arrays.asList(line)))
-                            .parts(parts)
-                            .build());
+                if (groupNumber == null) {
+                    if (Arrays.stream(rows).anyMatch(s -> !s.isEmpty())) {
+                        groups.add(new ArrayList<>(List.of(line)));
+                        apply(rows, groups.size() - 1, parts);
+                    }
+                } else {
+                    groups.get(groupNumber).add(line);
+                    apply(rows, groupNumber, parts);
                 }
                 line = reader.readLine();
             }
             reader.close();
 
             System.out.println("--------------");
-            for (Group group : groups) {
-                System.out.println(group.getLines());
-//                group.getParts().stream().forEach(p -> p.stream().forEach(i -> System.out.println(i)));
+            int count = 0;
+            for (List<String> group : groups) {
+                if (group.size() > 1) {
+                    count++;
+                    System.out.println(group);
+                }
             }
+            System.out.println(count);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void apply(List<String> newValues, List<Set<String>> possible) {
+    private static void apply(String[] newValues, int groupNumber, List<Map<String, Integer>> parts) {
         int j = 0;
-        for (; j < possible.size(); j++) {
-            if (j >= newValues.size()) {
+        for (; j < parts.size(); j++) {
+            if (j >= newValues.length) {
                 return;
             }
-            possible.get(j).add(newValues.get(j));
+            if (!newValues[j].isEmpty()) {
+                parts.get(j).put(newValues[j], groupNumber);
+            }
         }
-        for (; j < newValues.size(); j++) {
-            possible.add(new HashSet<>(Arrays.asList(newValues.get(j))));
+        for (; j < newValues.length; j++) {
+            if (!newValues[j].isEmpty()) {
+                HashMap<String, Integer> map = new HashMap<>();
+                map.put(newValues[j], groupNumber);
+                parts.add(map);
+            }
         }
     }
 }
